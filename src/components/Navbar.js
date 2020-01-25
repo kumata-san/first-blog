3247e23cfd0841fc7b87f3f8b88bd3cfd76150e5
kumata-san <kumatasan2019@gmail.com>
2020-01-26 02:09:58 +0900

    change alt

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '188px' }} />
+              <img src={logo} alt="Kumata-blog" style={{ width: '188px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
